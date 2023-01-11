b7c86ffc0ba79bbe3857960fa25153f911b2c172
Akis Kostakis <akiskost97@outlook.com>
2023-01-11 18:44:20 +0200

    Fixed some services and repositories

--- acmeflix-app/src/main/java/com/acmeflix/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acmeflix.repository;
-
-import com.acmeflix.domain.enumeration.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
