ca44d4c633be1dc8687e7a6313ac5648a4fea8d4
Akis Kostakis <akiskost97@outlook.com>
2023-01-16 00:45:17 +0200

    Some comments

--- acmeflix-app/src/main/java/com/acmeflix/Configuration/DbControllerBackUp.java
+++ acmeflix-app/src/main/java/com/acmeflix/Configuration/DbControllerBackUp.java
@@ -1,11 +1,14 @@
 package com.acmeflix.Configuration;
 
 import com.acmeflix.base.AbstractLogComponent;
+import com.acmeflix.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,10 +18,13 @@ import java.util.concurrent.TimeUnit;
 @EnableScheduling
 public class DbControllerBackUp extends AbstractLogComponent {
 
+    @Autowired
+    UserRepository userRepository;
 
     //@Scheduled(cron = "0 0 0/24 * * ?") //Every 24 hours
-    @Scheduled(cron = "0 0/1 * * * ?") //Every min for example
-    public void backup() throws IOException {
+    //@Scheduled(cron = "0 0/1 * * * ?") //Every min for example
+    @Scheduled(cron = "0/20 * * * * ?") //Every 10sec for example
+    public void backupUsers() throws IOException {
         Date date = new Date();
         logger.trace("Starting backup of DB: {}", date);
 
@@ -30,8 +36,19 @@ public class DbControllerBackUp extends AbstractLogComponent {
 
         String tables = "USERS"; //Make sure to include all the tables
 
-        File file = new File(rootPath + fileName);
 
-        file.createNewFile();
+        //Maybe try to use mysqldump, although the user must have installed it on his pc
+
+//        File file = new File(rootPath + fileName);
+//        file.createNewFile();
+//
+//        FileWriter f = new FileWriter(file);
+//
+//
+//        var a = userRepository.findAll();
+//
+//        f.write(a.toString());
+//        f.flush();
+//        f.close();
     }
 }
