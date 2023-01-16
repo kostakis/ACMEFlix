9a9a99e74cedba97925b803dc83be2e9b71fce2b
katlask810 <kat_lask@yahoo.gr>
2023-01-17 01:51:04 +0200

    GenerateMovies and TVShows

--- acmeflix-app/src/main/java/com/acmeflix/bootstrap/GenerateTvShows.java
+++ acmeflix-app/src/main/java/com/acmeflix/bootstrap/GenerateTvShows.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class GenerateTvShows extends AbstractLogComponent implements CommandLineRunner {
 
     private final TvShowService tvShowService;
+    private final TvShowService tvShowService2;
 
     @Override
     public void run(String... args) throws Exception {
@@ -60,9 +61,106 @@ public class GenerateTvShows extends AbstractLogComponent implements CommandLine
         tvShow.setCategory(Category.COMEDY);
         tvShow.setRating(9.0f);
 
+        TvShowEpisodes tvShowEpisodes9 = new TvShowEpisodes();
+        tvShowEpisodes9.setEpisode(1);
+        tvShowEpisodes9.setSeason(1);
+        tvShowEpisodes9.setDuration(75);
+
+        TvShowEpisodes tvShowEpisodes8 = new TvShowEpisodes();
+        tvShowEpisodes8.setEpisode(2);
+        tvShowEpisodes8.setSeason(1);
+        tvShowEpisodes8.setDuration(80);
+
+        TvShowEpisodes tvShowEpisodes7 = new TvShowEpisodes();
+        tvShowEpisodes7.setEpisode(1);
+        tvShowEpisodes7.setSeason(2);
+        tvShowEpisodes7.setDuration(80);
+
+        TvShowEpisodes tvShowEpisodes6 = new TvShowEpisodes();
+        tvShowEpisodes6.setEpisode(2);
+        tvShowEpisodes6.setSeason(2);
+        tvShowEpisodes6.setDuration(80);
+
+
+        TvShow tvShow2 = new TvShow();
+        tvShow2.setTvShowName("Gossip Girl");
+        tvShow2.setTvShowYear(2000);
+        tvShow2.setCategory(Category.DRAMA);
+        tvShow2.setRating(10f);
+
+        TvShowEpisodes tvShowEpisodes10 = new TvShowEpisodes();
+        tvShowEpisodes10.setEpisode(3);
+        tvShowEpisodes10.setSeason(1);
+        tvShowEpisodes10.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes11 = new TvShowEpisodes();
+        tvShowEpisodes11.setEpisode(2);
+        tvShowEpisodes11.setSeason(1);
+        tvShowEpisodes11.setDuration(64);
+
+        TvShowEpisodes tvShowEpisodes12 = new TvShowEpisodes();
+        tvShowEpisodes12.setEpisode(1);
+        tvShowEpisodes12.setSeason(1);
+        tvShowEpisodes12.setDuration(62);
+
+
+        TvShow tvShow3 = new TvShow();
+        tvShow3.setTvShowName("Ginny And Georgia");
+        tvShow3.setTvShowYear(2022);
+        tvShow3.setCategory(Category.ROMANCE);
+        tvShow3.setRating(8.2f);
+
+
+        TvShowEpisodes tvShowEpisodes19 = new TvShowEpisodes();
+        tvShowEpisodes19.setEpisode(3);
+        tvShowEpisodes19.setSeason(2);
+        tvShowEpisodes19.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes18 = new TvShowEpisodes();
+        tvShowEpisodes18.setEpisode(2);
+        tvShowEpisodes18.setSeason(2);
+        tvShowEpisodes18.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes17 = new TvShowEpisodes();
+        tvShowEpisodes17.setEpisode(1);
+        tvShowEpisodes17.setSeason(2);
+        tvShowEpisodes17.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes16 = new TvShowEpisodes();
+        tvShowEpisodes16.setEpisode(34);
+        tvShowEpisodes16.setSeason(1);
+        tvShowEpisodes16.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes15 = new TvShowEpisodes();
+        tvShowEpisodes15.setEpisode(3);
+        tvShowEpisodes15.setSeason(1);
+        tvShowEpisodes15.setDuration(60);
+
+        TvShowEpisodes tvShowEpisodes14 = new TvShowEpisodes();
+        tvShowEpisodes14.setEpisode(2);
+        tvShowEpisodes14.setSeason(1);
+        tvShowEpisodes14.setDuration(64);
+
+        TvShowEpisodes tvShowEpisodes13 = new TvShowEpisodes();
+        tvShowEpisodes13.setEpisode(1);
+        tvShowEpisodes13.setSeason(1);
+        tvShowEpisodes13.setDuration(62);
+
+
+        TvShow tvShow4 = new TvShow();
+        tvShow4.setTvShowName("Rick and Morty");
+        tvShow4.setTvShowYear(2000);
+        tvShow4.setCategory(Category.ANIMATION);
+        tvShow4.setRating(9.9f);
+
+
         tvShow.setEpisode(List.of(tvShowEpisodes,tvShowEpisodes2,tvShowEpisodes3,tvShowEpisodes4,tvShowEpisodes5));
+        tvShow2.setEpisode(List.of(tvShowEpisodes6,tvShowEpisodes7,tvShowEpisodes8,tvShowEpisodes9));
+        tvShow3.setEpisode(List.of(tvShowEpisodes10,tvShowEpisodes11,tvShowEpisodes12));
+        tvShow4.setEpisode(List.of(tvShowEpisodes13,tvShowEpisodes14,tvShowEpisodes16,tvShowEpisodes17,tvShowEpisodes18,tvShowEpisodes19));
 
         tvShowService.create(tvShow);
+//        tvShowService2.create(tvShow2);
 
     }
 }
